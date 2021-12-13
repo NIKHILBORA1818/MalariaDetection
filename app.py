419a911e449e1f2d2cd9319d254a58e8b0ecfb49
Pratik Mehkarkar <pratikmehkarkar@gmail.com>
2021-12-13 13:53:55 -0800

    Updated app.py
    Removed the unnecessary packages and changed the path for folder

--- app.py
+++ app.py
@@ -21,27 +21,27 @@ app = Flask(__name__)
 # Model saved with Keras model.save()
 MODEL_PATH = 'my_model.h5'
 
-#Load your trained model
-model = load_model(MODEL_PATH)         # Necessary to make everything ready to run on the GPU ahead of time
+# Load your trained model
+model = load_model(MODEL_PATH) 
 print('Model loaded. Start serving...')
+
 
 # You can also use pretrained model from Keras
 # Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#print('Model loaded. Check http://127.0.0.1:5000/')
+# from keras.applications.resnet50 import ResNet50
+# model = ResNet50(weights='imagenet')
+# print('Model loaded. Check http://127.0.0.1:5000/')
 
 
 def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(50,50)) #target_size must agree with what the trained model expects!!
+    img = image.load_img(img_path, target_size=(50, 50))  # target_size must agree with what the trained model expects!!
 
     # Preprocessing the image
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
 
-   
     preds = model.predict(img)
-    pred = np.argmax(preds,axis = 1)
+    pred = np.argmax(preds, axis=1)
     return pred
 
 
@@ -60,12 +60,12 @@ def upload():
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
-            basepath, '/Users/nikhilbora/Documents/malariadetection/uploads', secure_filename(f.filename))
+            basepath, '/Users/pratikmehkarkar/MS in CS/Sem-4/Deep Learning/MaleriaDetection-FinalProject/malariadetection-main/uploads', secure_filename(f.filename))
         f.save(file_path)
 
         # Make prediction
         pred = model_predict(file_path, model)
-        os.remove(file_path)#removes file from the server after prediction has been returned
+        os.remove(file_path)  # removes file from the server after prediction has been returned
 
         # Arrange the correct return according to the model. 
         # In this model 1 is Pneumonia and 0 is Normal.
@@ -77,10 +77,8 @@ def upload():
             return str2
     return None
 
-    #this section is used by gunicorn to serve the app on Heroku
+    # this section is used by gunicorn to serve the app on Heroku
+
+
 if __name__ == '__main__':
-        app.run()
-    #uncomment this section to serve the app locally with gevent at:  http://localhost:5000
-    # Serve the app with gevent 
-    #http_server = WSGIServer(('', 5000), app)
-    #http_server.serve_forever()
+    app.run()
